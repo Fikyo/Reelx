5f18191829a6388b14d6cbff962e246a1837b0ed
Vamsikeshwaran M <vamsikeshwaranm@Vamsikeshwarans-MacBook-Pro.local>
2025-04-14 13:08:01 +0530

    Created reelx init file

---
+++ src/reelx/__init__.py
@@ -0,0 +1,7 @@
+"""
+ReelX - A tool for transforming horizontal videos to vertical format using AI
+"""
+
+from reelx.config.configuration import Config
+from reelx.processors.video_processor import VideoProcessor
+from reelx.utils.printer import Printer
